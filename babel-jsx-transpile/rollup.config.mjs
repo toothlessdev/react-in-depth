6d8795bdd6f56e5edf5df0290384d29823cb5ef6
toothlessdev <toothless042@gmail.com>
2024-07-26 23:44:33 +0900

    chore: rollup 설정

---
+++ babel-jsx-transpile/rollup.config.mjs
@@ -0,0 +1,25 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+import _package from "./package.json" assert { type: "json" };
+
+export default {
+    input: _package.main,
+    output: [
+        {
+            file: "dist/bundle.js",
+            format: "cjs",
+        },
+    ],
+    plugins: [
+        nodeResolve(),
+        commonjs(),
+        babel({
+            babelHelpers: "bundled",
+            babelrc: true,
+        }),
+        json(),
+    ],
+};
